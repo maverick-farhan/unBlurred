b73f9bae1e55059abf02c7b8ec1a7e9527cecee2
maverick-farhan <mdfarhanofficial@gmail.com>
2024-05-10 15:23:39 +0530

    added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BlueFlame: Truth Trust and Beyond</title>
+</head>
+<body>
+   <h1>One Place Destination to know the Truth</h1> 
+   <h1>We are ONE, Stand For Truth, Honesty Law and Order, against Falsehood,racism, discrimination, and hatred and Evil in the society in modern era.</h1>
+   <h1>We Pass the blueFlame ahead to next gen.</h1>
+</body>
+</html>
